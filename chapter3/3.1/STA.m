ffc27cc63a334c220b82913dedce6501386de792
Clark Xu <yilinxu@brandeis.edu>
2023-02-28 23:09:18 -0500

    update 3.1

--- chapter3/3.1/STA.m
+++ chapter3/3.1/STA.m
@@ -1,4 +1,39 @@
-old = [3,3,2,2,1,1,3,3];
+function [sta, tcorr] = STA(Iapp, spikes, dt, tminus, tplus)
+    
+    if (~exist('tminus', 'var'))
+        tminus = 0.075;
+        nminus = tminus/dt;
+    else
+        nminus = tplus/dt;
+    end
 
-new_vector = expandbin(old, 2,1);
-disp(new_vector);
+    if (~exist('tplus', 'var'))
+        tplus = 0.025;
+        nplus = tplus/dt;
+    else
+        nplus = tplus/dt;
+    end
+
+    % number of time bins before and after a spike
+    %nminus = tminus/dt;
+    %nplus = tplus/dt;    
+    
+    % time window
+    tcorr = -nminus*dt :dt: nplus*dt;
+
+    sta = zeros(size(tcorr));
+    pks = find(spikes);
+    Nspike = length(pks);
+    Nignored = 0;
+    
+    for i=1:Nspike
+        if pks(i)<tminus/dt || length(Iapp)-pks(i)<tplus/dt
+            Nignored = Nignored+1;
+        else
+            sta = sta + Iapp(pks(i)-tminus/dt : pks(i)+tplus/dt);
+        end
+    end
+    
+    % take the average
+    sta = sta/(Nspike-Nignored);
+end
--- chapter3/3.1/main.m
+++ chapter3/3.1/main.m
@@ -10,19 +10,16 @@ b = 0.5e-9;
 tau_SRA = 0.050;
 
 %% set up vectors
-Nblock = 400;   % number of time blocks
+Nblock = 4e5;   % number of time blocks
 block = 5e-3;   % each time block is 5ms
 
 % applied current, random within ±0.5nA
 Iapp_rand = (1e-9).*rand(1,Nblock) - 0.5e-9; 
-
 dt = 2e-5;  % 0.02ms, time-bin of neuron
 new_dt = 0.001; % 1ms, only interested in changes on a time-scale of 1ms or more
 tvec = 0:dt:(block*Nblock-dt); 
 Nt = length(tvec);
-disp(Nt);
 Iapp = zeros(size(tvec));
-
 ratio = round(block/dt);   % times new vector greater than old
 
 for i=1:Nblock
@@ -35,7 +32,7 @@ Ntrial = 1;
 V = zeros(Ntrial, Nt);  % Membrane Potential
 V(:,1)=E_l; % initialize membrane potential
 I_SRA = zeros(Ntrial,Nt);
-
+spike = zeros(size(V));
 
 for i = 2:Nt-1
     dI_SRA = (a*(V(1,i-1)-E_l)-I_SRA(1,i-1))*dt/tau_SRA;
@@ -48,19 +45,17 @@ for i = 2:Nt-1
     % spike occurs
     if V(1,i)>V_th
         V(1,i)=V_reset;  % reset membrane potential
-
+        spike(1, i) = 1;
         % increment I_SRA
         I_SRA(1,i)=I_SRA(1,i)+b;
                         
     end    
 end
 
-new_V = expandbin(V, dt, new_dt);
-new_tvec = expandbin(tvec, dt, new_dt);
-plot(new_tvec,new_V)
+Iapp = expandbin(Iapp, dt, new_dt);
+spike = expandbin(spike, dt, new_dt);
+spike(find(spike)) = 1;   % replace fractions with a "1" for a spike
+[sta, tcorr] = STA(Iapp, spike, new_dt);
 
 figure(2)
-plot(tvec, V)
-
-c = findpeaks(new_V);
-disp(c)
+plot(tcorr,sta)
